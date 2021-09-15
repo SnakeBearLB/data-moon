36e756c7d5cecfeab6df9372855f7ca5871da9e2
SnakeBearLB <79704111+SnakeBearLB@users.noreply.github.com>
2021-09-15 13:25:14 -0400

    user can input name and historical event is returned

--- script.js
+++ script.js
@@ -1,30 +1,27 @@
-const settings = {
-	"async": true,
-	"crossDomain": true,
-	"url": "https://numbersapi.p.rapidapi.com/113/trivia?json=true&notfound=floor&fragment=true",
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "numbersapi.p.rapidapi.com",
-		"x-rapidapi-key": "412f149179msh81d913e7b508961p1f989djsn4fcf5b0efc84"
-	}
-};
 
 
-$.ajax(settings).done(function (response) {
-	console.log(response);
-});
+
+
 
 
 const $name = $('input[type=text]');
-userInput = '';
-const nameNums = [];
-const binaryName = [];
+const $yearFactCont = $('.bin-name-container');
 
 
-// if you click it, it will take your identity 
+let sumName;
+
+// if you submit your name, it will take your identity 
 $('form').on('submit', (e) => {
+
 	e.preventDefault();
+
+	$yearFactCont.empty();
+
+	const nameNums = [];
+	const binaryName = [];
+
 	userInput = $name.val();
+
 	$name.val('');
 
 	// convert each character of name to ascii number
@@ -33,6 +30,10 @@ $('form').on('submit', (e) => {
 		nameNums.push(char.charCodeAt(0));
 	});
 
+	// add all numbers in nameNums
+	sumName = nameNums.reduce((acc, num) => acc + num).toString();
+	console.log(sumName);
+
 	// name in binary
 	nameNums.forEach((num) => {
 		const holdBin = num.toString(2);
@@ -40,9 +41,43 @@ $('form').on('submit', (e) => {
 	});
 	console.log(binaryName);
 
-	const displayName = $('<p>');
-	$('form').text(binaryName);
+	// display each char in name as binary equivelant 
+	// const $displayName = $('<p>');
+	// $yearFactCont.append($displayName.text(`Your name in binary:  ${binaryName}`));
+
+	// const changeSett =
+	// console.log(settings['url'])
+
+	// get year fact
+	const settings = {
+		"async": true,
+		"crossDomain": true,
+		"url": `https://numbersapi.p.rapidapi.com/${sumName}/year?json=true&fragment=true`,
+		"method": "GET",
+		"headers": {
+			"x-rapidapi-host": "numbersapi.p.rapidapi.com",
+			"x-rapidapi-key": "412f149179msh81d913e7b508961p1f989djsn4fcf5b0efc84"
+		}
+	};
+
+
+	$.ajax(settings).done(function (response) {
+		console.log(response)
+		renderYearFact(response);
+	});
 });
+
+// function to render 
+function renderYearFact(data) {
+	const calcNameTxt = `My calculated name = ${sumName}`;
+	const $calcNamePar = $('<p>');
+	$yearFactCont.append($calcNamePar.text(calcNameTxt));
+	const yearFactTemp = `Year ${sumName}: `; 
+
+	// condition to alternate between date and text properties
+	const $yearFactPar = $('<p>');
+	$yearFactCont.append($yearFactPar.text(`${yearFactTemp} ${data.text}`));
+}
 
 
 
