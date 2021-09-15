5158080827fda85ac499f017f0a77fe506851d5e
SnakeBearLB <79704111+SnakeBearLB@users.noreply.github.com>
2021-09-14 21:45:56 -0400

    displays name in binary

--- script.js
+++ script.js
@@ -16,18 +16,33 @@ $.ajax(settings).done(function (response) {
 
 
 const $name = $('input[type=text]');
+userInput = '';
+const nameNums = [];
+const binaryName = [];
 
 
-
-
+// if you click it, it will take your identity 
 $('form').on('submit', (e) => {
 	e.preventDefault();
 	userInput = $name.val();
-	console.log(userInput);
 	$name.val('');
-	const $nameDisplay = $('<p>');
-	
-  $('.container').append($nameDisplay.text(userInput));
 
+	// convert each character of name to ascii number
+	const nameArr = userInput.split('');
+	nameArr.forEach((char) => {
+		nameNums.push(char.charCodeAt(0));
+	});
+
+	// name in binary
+	nameNums.forEach((num) => {
+		const holdBin = num.toString(2);
+		binaryName.push(holdBin);
+	});
+	console.log(binaryName);
+
+	const displayName = $('<p>');
+	$('form').text(binaryName);
 });
+
+
 
